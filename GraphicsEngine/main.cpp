3bb0ebb348252f1e2f5df216560af4d37bc775e2
Steven Iefel Flogio <69651154+iamstevenflogio@users.noreply.github.com>
2025-04-30 21:57:54 +0800

    completed the spinninng cube

--- GraphicsEngine/main.cpp
+++ GraphicsEngine/main.cpp
@@ -33,6 +33,7 @@ public:
 private:
     mesh meshCube;
     mat4x4 matProj;
+    float fTheta;
 
     void MultiplyMatrixVector(vec3d& i, vec3d& o, mat4x4& m)
     {
@@ -84,7 +85,7 @@ public:
         float fNear = 0.1f;
         float fFar = 1000.0f;
         float fFov = 90.0f;
-        float fAspectRatio = (float)ScreenHeight() / (float)ScreenWidth();
+        float fAspectRatio = (float)ScreenWidth() / (float)ScreenHeight();
         float fFovRad = 1.0f / tanf(fFov * 0.5f / 180.0f * 3.14159f);
 
         matProj.m[0][0] = fAspectRatio * fFovRad;
@@ -101,36 +102,68 @@ public:
 
         Fill(0, 0, ScreenWidth(), ScreenHeight(), PIXEL_SOLID, FG_BLACK);
 
+        mat4x4 matRotZ, matRotX;
+        fTheta += 1.0f * fElapsedTime;
+
+        // Rotation Z
+        matRotZ.m[0][0] = cosf(fTheta);
+        matRotZ.m[0][1] = sinf(fTheta);
+        matRotZ.m[1][0] = -sinf(fTheta);
+        matRotZ.m[1][1] = cosf(fTheta);
+        matRotZ.m[2][2] = 1;
+        matRotZ.m[3][3] = 1;
+
+        // Rotation X
+        matRotX.m[0][0] = cosf(fTheta * 0.5f);
+        matRotX.m[0][1] = sinf(fTheta * 0.5f);
+        matRotX.m[1][0] = -sinf(fTheta * 0.5f);
+        matRotX.m[1][1] = cosf(fTheta * 0.5f);
+        matRotX.m[2][2] = 1;
+        matRotX.m[3][3] = 1;
+
         // Draw Triangles
         for (auto tri : meshCube.tris)
         {
-            triangle triProjected, triTranslated;
+            triangle triProjected, triTranslated, triRotatedZ, triRotatedZX;
+            
+            MultiplyMatrixVector(tri.p[0], triRotatedZ.p[0], matRotZ);
+            MultiplyMatrixVector(tri.p[1], triRotatedZ.p[1], matRotZ);
+            MultiplyMatrixVector(tri.p[2], triRotatedZ.p[2], matRotZ);
 
-            triTranslated = tri;
-            triTranslated.p[0].z = tri.p[0].z + 3.0f;
-            triTranslated.p[1].z = tri.p[1].z + 3.0f;
-            triTranslated.p[1].z = tri.p[2].z + 3.0f;
+            MultiplyMatrixVector(triRotatedZ.p[0], triRotatedZX.p[0], matRotX);
+            MultiplyMatrixVector(triRotatedZ.p[1], triRotatedZX.p[1], matRotX);
+            MultiplyMatrixVector(triRotatedZ.p[2], triRotatedZX.p[2], matRotX);
 
-            MultiplyMatrixVector(tri.p[0], triProjected.p[0], matProj);
-            MultiplyMatrixVector(tri.p[0], triProjected.p[0], matProj);
-            MultiplyMatrixVector(tri.p[0], triProjected.p[0], matProj);
-             
-			// Scale into view
-			triProjected.p[0].x += 1.0f; triProjected.p[0].y += 1.0f;
-			triProjected.p[1].x += 1.0f; triProjected.p[1].y += 1.0f;
-			triProjected.p[2].x += 1.0f; triProjected.p[2].y += 1.0f;
-			triProjected.p[0].x *= 0.5f * (float)ScreenWidth();
-			triProjected.p[0].y *= 0.5f * (float)ScreenHeight();
-			triProjected.p[1].x *= 0.5f * (float)ScreenWidth();
-			triProjected.p[1].y *= 0.5f * (float)ScreenHeight();
-			triProjected.p[2].x *= 0.5f * (float)ScreenWidth();
-			triProjected.p[2].y *= 0.5f * (float)ScreenHeight();
+            triTranslated = triRotatedZX;
+            triTranslated.p[0].z = triRotatedZX.p[0].z + 3.0f;
+            triTranslated.p[1].z = triRotatedZX.p[1].z + 3.0f;
+            triTranslated.p[2].z = triRotatedZX.p[2].z + 3.0f;
+
+            // triangle daw ito pre
+            MultiplyMatrixVector(triTranslated.p[0], triProjected.p[0], matProj);
+            MultiplyMatrixVector(triTranslated.p[1], triProjected.p[1], matProj);
+            MultiplyMatrixVector(triTranslated.p[2], triProjected.p[2], matProj);
+            
+            //Scale into view
+            triProjected.p[0].x += 1.0f; triProjected.p[0].y += 1.0f;
+            triProjected.p[1].x += 1.0f; triProjected.p[1].y += 1.0f;
+            triProjected.p[2].x += 1.0f; triProjected.p[2].y += 1.0f;
 
 
-            DrawTriangle(triProjected.p[0].x, triProjected.p[0].y,
+            triProjected.p[0].x *= 0.5f * (float)ScreenWidth();
+            triProjected.p[0].y *= 0.5f * (float)ScreenHeight();
+            triProjected.p[1].x *= 0.5f * (float)ScreenWidth();
+            triProjected.p[1].y *= 0.5f * (float)ScreenHeight();
+            triProjected.p[2].x *= 0.5f * (float)ScreenWidth();
+            triProjected.p[2].y *= 0.5f * (float)ScreenHeight();
+
+
+            DrawTriangle(
+                triProjected.p[0].x, triProjected.p[0].y,
                 triProjected.p[1].x, triProjected.p[1].y,
                 triProjected.p[2].x, triProjected.p[2].y,
                 PIXEL_SOLID, FG_WHITE);
+
         }
 
         return true;
